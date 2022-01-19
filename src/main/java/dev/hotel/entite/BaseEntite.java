04b6edf28e3cfc8eedc5b4b93a1687d14cca6977
Elisa Lambert <elisa.lambertrondouin@gmail.com>
2022-01-19 19:32:54 +0100

    resolve

--- src/main/java/dev/hotel/entite/BaseEntite.java
+++ src/main/java/dev/hotel/entite/BaseEntite.java
@@ -18,7 +18,8 @@ public abstract class BaseEntite {
 
     public void setId(Integer id) {
         this.id = id;
-    } 
+    }
+    
     public String testConfilct() {
     	
     	System.out.println("lllmlll");
